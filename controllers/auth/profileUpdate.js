8981ae4086ddfce47df31bbe6851a42f970f8a44
its-robii <robiulhassanrobi33@gmail.com>
2025-04-15 12:29:07 +0600

    sgdcg

--- controllers/auth/profileUpdate.js
+++ controllers/auth/profileUpdate.js
@@ -1,5 +1,18 @@
-const profileUpdate = (req, res) => {
-    res.status(200).send("profileUpdate")
+const userSchema = require("../../modal/userSchema");
+
+const profileUpdate = async (req, res) => {
+
+    const {fullName, password, avatar} = req.body;
+
+    const updateFileds = {};
+
+    if(fullName) updateFileds.fullName = fullName;
+    if(password) updateFileds.password = password;
+    if(avatar) updateFileds.avatar = avatar;
+
+    const existngUser = await userSchema.findByIdAndUpdate("", updateFileds, {new : true})
+
+    res.status(200).send(updateFileds)
 }
 
 module.exports = {profileUpdate}

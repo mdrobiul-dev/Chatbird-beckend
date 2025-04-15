aad80d0c67e32167f66dac54af9074c910410157
its-robii <robiulhassanrobi33@gmail.com>
2025-04-15 12:21:49 +0600

    dbfdhbhhhvj

---
+++ controllers/auth/profileUpdate.js
@@ -0,0 +1,5 @@
+const profileUpdate = (req, res) => {
+    res.status(200).send("profileUpdate")
+}
+
+module.exports = {profileUpdate}
--- router/api/auth.js
+++ router/api/auth.js
@@ -4,6 +4,7 @@ const login = require('../../controllers/auth/login');
 const emailvariefied = require('../../controllers/auth/emailVariefied');
 const { forgotPassword } = require('../../controllers/auth/forgetPassword');
 const resetPassword = require('../../controllers/auth/resetPassword');
+const { profileUpdate } = require('../../controllers/auth/profileUpdate');
 const authRout = express.Router()
 
 authRout.post("/registration", registration)
@@ -11,5 +12,6 @@ authRout.post("/emailvariefication", emailvariefied)
 authRout.post("/login", login)
 authRout.post("/forgetpassword", forgotPassword)
 authRout.post("/resetpassword/:randomString", resetPassword)
+authRout.post("profileupdate", profileUpdate)
 
 module.exports = authRout

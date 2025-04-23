76869a7024425d7fb4e4bfdd3626682d71f13b7a
its-robii <robiulhassanrobi33@gmail.com>
2025-04-23 20:26:41 +0600

    update

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,8 +1,8 @@
 const messageSend = async (req, res) => {
     try {
-     res.send("hello world")
+    return res.send("hello world")
     } catch (error) {
-     
+     res.status(500).send("server error")
     }
 }
 

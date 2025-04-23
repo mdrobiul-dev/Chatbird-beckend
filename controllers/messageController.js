256e0ede7661afa7d0637c8804b01f73952720bc
its-robii <robiulhassanrobi33@gmail.com>
2025-04-23 20:23:50 +0600

    update

---
+++ controllers/messageController.js
@@ -0,0 +1,5 @@
+const messageSend = async (req, res) => {
+     res.send("hello world")
+}
+
+module.exports =  { messageSend }
--- router/api/chat.js
+++ router/api/chat.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const {createConverstion, conversationList}= require('../../controllers/conversationController');
 const validUser = require('../../middlewears/authMiddlewear');
+const { messageSend } = require('../../controllers/messageController');
+
+
 const chatRoute = express.Router()
 
 chatRoute.post("/createconversation", validUser , createConverstion)
 chatRoute.get("/conversationlist", validUser, conversationList)
+chatRoute.post("/send", validUser, messageSend)
 
 module.exports = chatRoute

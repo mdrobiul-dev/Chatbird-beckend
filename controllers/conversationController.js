62f377798531bf567ebf18d9789afdf49be4943d
its-robii <robiulhassanrobi33@gmail.com>
2025-05-08 20:01:53 +0600

    conversation logic

--- controllers/conversationController.js
+++ controllers/conversationController.js
@@ -41,7 +41,7 @@ const createConverstion = async (req, res) => {
 
     res.status(200).send(conversation);
   } catch (error) {
-    res.status(500).send("Server error!");
+    res.status(500).send({ error: "server error" });
   }
 };
 
